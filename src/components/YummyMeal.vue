d00d416eb8a54326d858fd1401a9d94f1bfd8599
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-10-04 18:04:33 +0200

    init

---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menu-item">
+    <h2>{{ name }}</h2>
+    <strong>{{ returnPrice }}</strong
+    ><br />
+    <button @click="addToCart">Add to cart</button>
+  </div>
+</template>
+
+<script>
+// import
+import { computed } from "vue";
+
+// export
+export default {
+  // Props
+  props: {
+    name: String,
+    price: Number,
+  },
+
+  setup(props, { emit }) {
+    // Function
+    const addToCart = () => emit("addToCart", props.name);
+    const returnPrice = computed(() => `€ ${props.price.toFixed(2)}`);
+
+    // Return
+    return { addToCart, returnPrice };
+  },
+};
+</script>
+
+<style scoped>
+.menu-item {
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <h1>{{ shopName }}</h1>
+
+    <YummyMeal
+      v-for="item in meals"
+      :name="item.name"
+      :price="item.price"
+      @addToCart="addItemToCart"
+    />
+  </main>
+</template>
+
+<script>
+// import
+import { ref, reactive, watch } from "vue";
+import YummyMeal from "../components/YummyMeal.vue";
+
+// export
+export default {
+  setup() {
+    // Variable
+    const shopName = ref("Yummy Meals Shop!");
+
+    const meals = reactive([
+      { name: "Hamburger", price: 8 },
+      { name: "Cheeseburger", price: 6 },
+      { name: "Carrot", price: 1 },
+      { name: "Chips", price: 2 },
+    ]);
+
+    const cart = reactive([]);
+
+    // Function
+    const addItemToCart = (item) => cart.push(item);
+
+    watch([() => [...cart]], (newValue, oldValue) =>
+      console.log(newValue, oldValue)
+    );
+
+    // Return
+    return { addItemToCart, shopName, meals };
+  },
+
+  // Components
+  components: { YummyMeal },
+};
+</script>
